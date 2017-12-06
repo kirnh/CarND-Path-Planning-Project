c44268e43f819893e3bb763918b562b581c3fb1b
Kiran Hegde <kiran.hegde121@gmail.com>
2017-12-06 09:40:42 +0530

    Initial commit, contains boiler plate code from Udacity

--- src/main.cpp
+++ src/main.cpp
@@ -242,7 +242,6 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
-
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
